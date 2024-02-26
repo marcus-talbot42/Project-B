d0c5929f70892277595eb209c85cc32a25a13ef4
Marcus Talbot <marcus.talbot@42.nl>
2024-02-26 15:31:29 +0100

    Remove unnecessary code
    - Removed unnecessary code that only existed for the purpose of setting up a test-project.

--- ProjectBTest/UnitTest1.cs
+++ ProjectBTest/UnitTest1.cs
@@ -1,5 +1,3 @@
-using ProjectB.io;
-
 namespace ProjectBTest;
 
 [TestClass]
@@ -8,14 +6,5 @@ public class UnitTest1
     [TestMethod]
     public void TestAdditionCalculator()
     {
-        AdditionCalculator additionCalculator = new();
-        Assert.AreEqual(5, additionCalculator.Process(2, 3));
-    }
-
-    [TestMethod]
-    public void TestAdditionCalculator_ShouldNotBeEqual()
-    {
-        AdditionCalculator additionCalculator = new();
-        Assert.AreNotEqual(5, additionCalculator.Process(3, 3));
     }
 }
