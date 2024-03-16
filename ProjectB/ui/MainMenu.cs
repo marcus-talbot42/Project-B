71e6690b9dd6a11b556e372007a1c67c28abe0e6
Marcus Talbot <marcus.talbot@42.nl>
2024-03-16 14:38:21 +0100

    Create MainMenu
    - Create MainMenu, containing the ShowMenu-method.

---
+++ ProjectB/ui/MainMenu.cs
@@ -0,0 +1,19 @@
+namespace ProjectB.ui;
+
+public class MainMenu
+{
+
+    public void ShowMenu()
+    {
+        Console.WriteLine("""
+                          Welkom!
+                          
+                          Kies een optie uit het onderstaande menu om verder te gaan:
+                          
+                          1) Log in als gast
+                          2) Log in als medewerker
+                          3) Change language to English
+                          """);
+    }
+    
+}
