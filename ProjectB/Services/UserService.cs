01f81e0e7c39c1978f632e1683e63702a300691e
Marcus Talbot <marcus.talbot@42.nl>
2024-03-29 14:21:54 +0100

    Create UserService.cs
    - Provides a central point from where database operations can be safely mixed with business logic.

---
+++ ProjectB/Services/UserService.cs
@@ -0,0 +1,41 @@
+using ProjectB.Exceptions;
+using ProjectB.Models;
+using ProjectB.Repositories;
+
+namespace ProjectB.Services;
+
+public class UserService: IService<User, string>
+{
+
+    private static readonly Lazy<UserService> Lazy = new (() => new UserService());
+    public static UserService Instance => Lazy.Value;
+
+    private readonly UserRepository _repository;
+
+    private UserService() => _repository = UserRepository.Instance;
+
+    public void Test()
+    {
+        Console.WriteLine("Got the UserService...");
+        Console.WriteLine(_repository.FindAll());
+    }
+
+    public void Create(User entity)
+    {
+        if (_repository.FindById(entity.GetId()) != null)
+        {
+            throw new PrimaryKeyConstraintException($"Primary Key 'User._username' violated. A User with ID {entity.GetId()} already exists.");
+        }
+        _repository.Save(entity);
+    }
+
+    public void Update(User entity, string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
