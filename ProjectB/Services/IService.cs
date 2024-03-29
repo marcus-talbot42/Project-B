f560c741c94406cec024504de768f99405d93a34
Marcus Talbot <marcus.talbot@42.nl>
2024-03-29 14:18:02 +0100

    Create IService.cs
    - Serves as the minimum requirement for any class to be considered a Service.

---
+++ ProjectB/Services/IService.cs
@@ -0,0 +1,19 @@
+using ProjectB.Models;
+
+namespace ProjectB.Services;
+
+public interface IService<TEntity, TId>
+    where TEntity : Entity<TId>
+{
+
+    void Create(TEntity entity);
+
+    void Update(TEntity entity) => Update(entity, entity.GetId());
+
+    void Update(TEntity entity, TId id);
+
+    void Delete(TId id);
+
+    void Delete(TEntity entity) => Delete(entity.GetId());
+
+}
