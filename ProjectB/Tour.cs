fda29a2cf2c036f9c3ae827a8fbf41d677939b1e
KaiHog <161350865+KaiHog@users.noreply.github.com>
2024-04-02 18:29:13 +0200

    Update Tour.cs
    changed participantcount to Icollection<participant>

--- ProjectB/Tour.cs
+++ ProjectB/Tour.cs
@@ -3,13 +3,13 @@ class Tour
     public DateTime Time { get; set; } // Time of the tour
     public string Location { get; set; } // Location of the tour
     public int Capacity { get; set; } // Maximum capacity of the tour
-    public int ParticipantsCount { get; set; } // Number of participants signed up for the tour
+    public ICollection<Participant> Participants { get; set; } // Collection of participants signed up for the tour
 
     public Tour(DateTime time, string location, int capacity)
     {
         Time = time;
         Location = location;
         Capacity = capacity;
-        ParticipantsCount = 0; // Initially, no participants signed up
+        Participants = new List<Participant>(); // Initialize participant collection
     }
 }
