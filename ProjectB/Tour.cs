142fb22df432d2818f2097ecedb360054787ad5e
KaiHog <kaihogendoorn16@hotmail.com>
2024-04-03 20:22:56 +0200

    Participant > Guest

--- ProjectB/Tour.cs
+++ ProjectB/Tour.cs
@@ -1,15 +1,16 @@
+using ProjectB.Models;
 class Tour
 {
     public DateTime Time { get; set; } // Time of the tour
     public string Location { get; set; } // Location of the tour
     public int Capacity { get; set; } // Maximum capacity of the tour
-    public ICollection<Participant> Participants { get; set; } // Collection of participants signed up for the tour
+    public ICollection<Guest> Participants { get; set; } // Collection of participants signed up for the tour
 
     public Tour(DateTime time, string location, int capacity)
     {
         Time = time;
         Location = location;
         Capacity = capacity;
-        Participants = new List<Participant>(); // Initialize participant collection
+        Participants = new List<Guest>(); // Initialize participant collection
     }
 }
