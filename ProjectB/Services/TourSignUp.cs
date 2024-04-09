b181a7049a5bb1804d0a078d45fca59b8d18fcab
KaiHog <kaihogendoorn16@hotmail.com>
2024-04-09 20:40:51 +0200

    Nog bezig

---
+++ ProjectB/Services/TourSignUp.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace ProjectB.Models
+{
+    public class TourSignUp
+    {
+        public ICollection<Tour> SignUps { get; set; }
+
+        public TourSignUp(ICollection<Tour> signUps)
+        {
+            SignUps = signUps;
+        }
+
+        public List<string> GetParticipants(DateTime tourTime)
+        {
+            List<string> participants = new List<string>();
+            foreach (var tour in SignUps)
+            
+            {
+            
+                foreach (var participant in tour.Participants)
+                {
+                    participants.Add(participant.GetId());
+                }
+            }
+
+            return participants;
+        }
+    }
+}
