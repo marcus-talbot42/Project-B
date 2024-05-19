84071c41cd6c73eeaf69b9ac87f0058d5396d5a7
Jeroen Faas <jeroen@espersolutions.nl>
2024-05-19 23:34:08 +0200

    Half commit

--- ProjectB/Models/Tour.cs
+++ ProjectB/Models/Tour.cs
@@ -9,34 +9,4 @@ public class Tour(DateTime start, int capacity = 13)
     public DateTime Start { get; set; } = start;
     public int Capacity { get; set; } = capacity;
     public Employee Employee { get; set; }
-
-    public DateTime GetTourTime()
-    {
-        return Start;
-    }
-
-    public int GetRemainingCapacity()
-    {
-        return Capacity - Participants.Count;
-    }
-
-    public Employee GetGuide()
-    {
-        return Employee;
-    }
-
-    public void SetGuide(Employee guide)
-    {
-        Employee = guide;
-    }
-
-    public ICollection<Guest> GetParticipants()
-    {
-        return Participants.ToImmutableList();
-    }
-
-    public int GetCapacity()
-    {
-        return Capacity;
-    }
 }
--- ProjectB/Services/TourService.cs
+++ ProjectB/Services/TourService.cs
@@ -6,6 +6,10 @@ namespace ProjectB.Services;
 
 public class TourService(TourRepository repository) : IService<Tour>
 {
+    public static readonly int MaxCapacity = 13;
+    public static readonly int TourDuration = 40;
+    public static readonly int DefaultTourInterval = 20;
+
     public void Create(Tour entity)
     {
         if (repository.Exists(entity)) {
@@ -44,5 +48,15 @@ public class TourService(TourRepository repository) : IService<Tour>
         tour.GetParticipants().Add(guest);
         repository.Save(tour);
         return true;
+    }
+
+    public int GetRemainingCapacity()
+    {
+        return Capacity - Participants.Count;
+    }
+
+    public ICollection<Guest> GetParticipants()
+    {
+        return Participants.ToImmutableList();
     }
 }
